717a4d1b6879bed999d5a3f5ba96987ca2354fe9
erikrasmussen23 <erik.rasmussen@hci.utah.edu>
2017-11-22 09:11:29 -0700

    GNOM6-325: Merge conflicts resolved

--- gnomex_ng/src/app/experiments/browse-experiments.component.ts
+++ gnomex_ng/src/app/experiments/browse-experiments.component.ts
@@ -30,7 +30,7 @@ import {DragDropHintComponent} from "../analysis/drag-drop-hint.component";
 
 @Component({
     selector: "experiments",
-    templateUrl: "./experiments.component.html",
+    templateUrl: "./browse-experiments.component.html",
     styles: [`
         .inlineComboBox {
             display: inline-block;
@@ -265,6 +265,8 @@ export class BrowseExperimentsComponent implements OnInit, OnDestroy, AfterViewI
             }
 
             this.experimentsService.emitExperimentOverviewList(response);
+            this.router.navigate(['/experiments',{outlets:{'browsePanel':'overview'}}]);
+
             setTimeout(()=>{
                 this.toggleButton.val("Collapse Projects");
                 this.treeModel.expandAll();
@@ -298,46 +300,46 @@ export class BrowseExperimentsComponent implements OnInit, OnDestroy, AfterViewI
             l.icon = "assets/group.png";
             // If there is a lab with no Project skip
             if (l.Project) {
-                if (!this.isArray(l.Project)) {
-                    l.items = [l.Project];
-                } else {
-                    l.items = l.Project;
-                }
-                for (var p of l.items) {
-                    p.icon = "assets/folder.png";
-                    p.labId = l.labId;
-                    p.id = p.idProject;
-                    p.parentid = l.id;
-                    if (p.Request) {
-                        if (!this.isArray(p.Request)) {
-                            p.items = [p.Request];
-                        } else {
-                            p.items = p.Request;
-                        }
-                        for (var r of p.items) {
-                            if (r) {
-                                if (r.label) {
-                                    var shortLabel = r.label.substring(0, (r.label.lastIndexOf("-")));
-                                    var shorterLabel = shortLabel.substring(0, shortLabel.lastIndexOf("-"));
-                                    r.label = shorterLabel;
-                                    this.experimentCount++;
-                                    r.id = r.idRequest;
-                                    r.parentid = p.id;
-                                    if (this.experimentCount % 100 === 0) {
-                                        console.log("experiment count " + this.experimentCount);
-                                    }
-                                } else {
-                                    console.log("label not defined");
+            if (!this.isArray(l.Project)) {
+                l.items = [l.Project];
+            } else {
+                l.items = l.Project;
+            }
+            for( var p of l.items) {
+                p.icon = "assets/folder.png";
+                p.labId = l.labId;
+                p.id = p.idProject;
+                p.parentid = l.id;
+                if (p.Request) {
+                    if (!this.isArray(p.Request)) {
+                        p.items = [p.Request];
+                    } else {
+                        p.items = p.Request;
+                    }
+                    for (var r of p.items) {
+                        if (r) {
+                            if (r.label) {
+                                var shortLabel = r.label.substring(0, (r.label.lastIndexOf("-")));
+                                var shorterLabel = shortLabel.substring(0, shortLabel.lastIndexOf("-"));
+                                r.label = shorterLabel;
+                                this.experimentCount++;
+                                r.id = r.idRequest;
+                                r.parentid = p.id;
+                                if (this.experimentCount % 100 === 0) {
+                                    console.log("experiment count " + this.experimentCount);
                                 }
                             } else {
-                                console.log("r is undefined");
+                                console.log("label not defined");
                             }
+                        } else {
+                            console.log("r is undefined");
                         }
-                    } else {
-                        console.log("");
                     }
+                } else {
+                    console.log("");
                 }
             }
+        }
         }
     };
 
@@ -384,7 +386,7 @@ export class BrowseExperimentsComponent implements OnInit, OnDestroy, AfterViewI
                 targetItem: this.targetItem,
                 showBillingCombo: this.showBillingCombo
 
-            }
+    }
         });
         this.reassignExperimentDialogRef.afterClosed()
             .subscribe(result => {
@@ -465,7 +467,7 @@ export class BrowseExperimentsComponent implements OnInit, OnDestroy, AfterViewI
                     'you do not have permission to access the member list for this lab. Please contact an administrator.', null)
                 .subscribe(
                     res => {
-                        this.resetTree();
+            this.resetTree();
                     }
                 );
         } else {
@@ -492,16 +494,16 @@ export class BrowseExperimentsComponent implements OnInit, OnDestroy, AfterViewI
                 useThisLabList = this.labList;
             } else {
                 useThisLabList = this.labs;
-            }
+    }
 
             this.createProjectDialogRef= this.dialog.open(CreateProjectComponent, {
                 data: {
                     labList: useThisLabList,
                     items: this.items,
                     selectedLabItem: this.selectedItem
-                }
-            });
         }
+            });
+    }
     }
 
     /**
@@ -513,7 +515,7 @@ export class BrowseExperimentsComponent implements OnInit, OnDestroy, AfterViewI
         this.deleteProjectDialogRef = this.dialog.open(DeleteProjectComponent, {
             data: {
                 selectedItem: this.selectedItem,
-            }
+    }
         });
     }
 
@@ -521,11 +523,11 @@ export class BrowseExperimentsComponent implements OnInit, OnDestroy, AfterViewI
         this.deleteExperimentDialogRef = this.dialog.open(DeleteExperimentComponent, {
             data: {
                 selectedExperiment: this.selectedExperiment
-            }
-        });
-
-
     }
+            });
+
+
+        }
     /**
      * A node is selected in the tree.
      * @param event
@@ -562,7 +564,7 @@ export class BrowseExperimentsComponent implements OnInit, OnDestroy, AfterViewI
 
             //Experiment
         } else {
-            //this.router.navigate(['/experiments',{outlets:{'browsePanel':[idRequest]}}]);
+            this.router.navigate(['/experiments',{outlets:{'browsePanel':[idRequest]}}]);
             this.disableNewProject = true;
             this.disableDeleteProject = true;
             this.experimentsService.getExperiment(this.selectedItem.data.idRequest).subscribe((response: any) => {
@@ -575,9 +577,6 @@ export class BrowseExperimentsComponent implements OnInit, OnDestroy, AfterViewI
             });
 
         }
-        this.experimentsService.selectedTreeNode = _.cloneDeep(this.selectedItem.data);
-
-
     }
 
     /**
