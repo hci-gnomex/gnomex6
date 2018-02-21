d1a62de1499433ab31b4b5e41b4a1b22cd1e0aae
erikrasmussen23 <erik.rasmussen@hci.utah.edu>
2018-02-21 16:19:12 -0700

    GNOM6-642: replaced the jqx-grid with ag-grid for experiments tab

--- gnomex_ng/src/app/experiments/browse-overview/experiments-browse.component.ts
+++ gnomex_ng/src/app/experiments/browse-overview/experiments-browse.component.ts
@@ -7,78 +7,53 @@ import {Subscription} from "rxjs/Subscription";
 import {DictionaryService} from "../../services/dictionary.service";
 import {ConstantsService} from "../../services/constants.service";
 import {Router} from "@angular/router";
+import {IconTextRendererComponent} from "../../util/grid-renderers/icon-text-renderer.component";
+import {IconRendererComponent} from "../../util/grid-renderers/icon-renderer.component";
+import {GridOptions} from "ag-grid/main";
 
 @Component({
     template: `
 
         <!--- <grid dataProvider={} > -->
         <div style="display:block; height:100%; width:100%;">
-            <GnomexStyledGrid 
-                    [selectionSetting]="SINGLE_ROW"
-                    (rowDoubleClicked)="forwardToExperiment($event)"
-                    [styleForTheme]="style">
-            </GnomexStyledGrid>
+            <ag-grid-angular
+                    class="ag-fresh"
+                    style="width: 100%; height: 90%;"
+                    (cellDoubleClicked)="forwardToExperiment($event)"
+                    [gridOptions]="gridOpt"
+                    [rowData]="rowData"
+                    [columnDefs]="columnDefs"
+                    [rowSelection]="rowSelection"
+                    [rowDeselection]="true"
+                    [enableSorting]="true"
+                    [enableColResize]="true">
+            </ag-grid-angular>
         </div>
     `
 })
 export class ExperimentsBrowseTab extends PrimaryTab implements OnInit,OnDestroy{
-    @ViewChild(GnomexStyledGridComponent) myGrid: GnomexStyledGridComponent;
     private selectedTreeNodeSubscript: Subscription;
     private filteredExperimentOverviewListSubscript: Subscription;
-    public readonly SINGLE_ROW: string = "singlerow";
-    public readonly style:string = "gnomex5_browse_experiment";
+    public readonly rowSelection: string = "single";
+    private gridOpt:GridOptions = {};
+
+
 
     name:string = "Experiments";
 
-
-
-    //Override
-    setState(){
-        this.myGrid.resizeIfNeeded();
-    }
-
-    private iconCellRenderer = (row: number, column: any, imgSource: any) =>{
-
-        return `<div style="display: block; text-align: left; padding: 0.3rem 0.5rem;">
-							<img src="` + imgSource +`" alt=""/>` +
-        `</div>`;
-    };
-    private experimentNumberCellsRenderer = (row: number, column: any, value: any): any => {
-        let imgSource = this.source.localdata[row].icon;
-        return `<div style="display: block; text-align: left; padding: 0.3rem 0.5rem;">
-							<img src="` + imgSource +`" alt=""/>` + value +
-            `</div>`;
-    };
-    private textCellsRenderer = (row: number, column: any, value: any): any => {
-        return `<div style="display: block; text-align: left; padding: 0.3rem 0.5rem;">` + value + `</div>`;
-    };
-
-
-    private columns: any[] = [
-        {text: "# ",        datafield: "requestNumber", 	width: "7%", cellsrenderer: this.experimentNumberCellsRenderer },
-        {text: "Date",      datafield: "requestCreateDateDisplay", 	width: "7%", cellsrenderer: this.textCellsRenderer},
-        {text: "Requester", datafield:"ownerFullName", width: "6%", cellsrenderer: this.textCellsRenderer},
-        {text: "Project",   datafield: "projectName", width: "8%", cellsrenderer: this.textCellsRenderer},
-        {text: "Experiment Kind", datafield:"experimentKind",  width: "31%", cellsrenderer: this.textCellsRenderer},
-        {text: "Microarray", datafield: "slideProductName",    width: "7%", cellsrenderer: this.textCellsRenderer},
-        {text: "Analysis?",datafield: "analysisChecked", 	  width: "7%", cellsrenderer: this.iconCellRenderer},
-        {text: "Analysis Names",   datafield: "analysisNames", width: "27%", cellsrenderer: this.textCellsRenderer},
+    columnDefs = [
+        {headerName: "#", editable: false, field: "requestNumber", width: 150, cellRendererFramework: IconTextRendererComponent},
+        {headerName: "Date", editable: false, field: "requestCreateDateDisplay", width:150},
+        {headerName: "Requester", editable: false, field: "ownerFullName", width: 200},
+        {headerName: "Project", editable: false, field: "projectName", width:150},
+        {headerName: "Experiment Type", editable:false, field:"experimentKind", width:300},
+        {headerName: "Microarray", editable:false, field:"slideProductName", width:200, },
+        {headerName: "Analysis?", editable:false, field:"analysisChecked", width:150, cellRendererFramework: IconRendererComponent},
+        {headerName: "Analysis Names", editable:false, field:"analysisNames",width:300}
     ];
 
-    private source = {
-        datatype: "json",
-        localdata: [],
-        datafields: [
-            {name: "requestNumber", type: "string"},
-            {name: "requestCreateDateDisplay", type: "string"},
-            {name: "ownerFullName", type: "string"},
-            {name: "projectName", type: "string"},
-            {name: "experimentKind", type: "string"},
-            {name: "slideProductName", type: "string"},
-            {name: "analysisChecked", type: "string"},
-            {name: "analysisNames", type: "string"},
-        ]
-    };
+    rowData:Array<any> = [];
+
     constructor(protected fb: FormBuilder, private experimentService:ExperimentsService,
                 private dictionary: DictionaryService, private appConstants: ConstantsService,
                 private router:Router) {
@@ -86,38 +61,29 @@ export class ExperimentsBrowseTab extends PrimaryTab implements OnInit,OnDestroy
     }
 
 
-
     ngOnInit(){
 
-        this.myGrid.setColumns(this.columns);
-        this.myGrid.setDataAdapterSource(this.source);
 
         this.filteredExperimentOverviewListSubscript = this.experimentService.getFilteredOverviewListObservable()
             .subscribe( data =>{
-                this.updateGridData(data);
+                this.rowData = data;
             });
         this.selectedTreeNodeSubscript = this.experimentService.getExperimentOverviewListSubject()
             .subscribe(data =>{
-                this.updateGridData(this.experimentService.experimentList);
+                this.rowData = this.experimentService.experimentList;
             });
 
     }
 
-
-
-
-    updateGridData(data:Array<any>):void{
-        this.source.localdata = Array.isArray(data) ? data :[data];
-        this.myGrid.setDataAdapterSource(this.source);
-        //this.myGrid.selectedrowindexes([]);
-
+    //Override
+    tabVisibleHook(){
+        this.gridOpt.api.sizeColumnsToFit();
     }
 
 
 
-    forwardToExperiment($event:any){
-
-        let rowData = $event.args.row.bounddata;
+    forwardToExperiment(event:any){
+        let rowData = event.data;
         let experimentNode = this.experimentService.experimentList
             .find(reqObj => reqObj.requestNumber === rowData.requestNumber);
         this.router.navigate(['/experiments',{outlets:{'browsePanel':[experimentNode.idRequest]}}]);
