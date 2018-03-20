9480185f20cd69ff46d7a59b66fe97cc945dc1d5
John Hofer <John.Hofer@hci.utah.edu>
2018-03-20 12:51:10 -0600

    Pruning extra imports in module

--- gnomex_ng/src/app/usersGroups/users-groups-tablist.module.ts
+++ gnomex_ng/src/app/usersGroups/users-groups-tablist.module.ts
@@ -1,35 +1,22 @@
 /*
  * Copyright (c) 2016 Huntsman Cancer Institute at the University of Utah, Confidential and Proprietary
  */
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {FormsModule,ReactiveFormsModule} from "@angular/forms";
+import { NgModule} from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { AgGridModule } from 'ag-grid-angular/main';
+import { AgGridEditorModule } from "../util/grid-editors/ag-grid-editor.module";
+import { AgGridRendererModule } from "../util/grid-renderers/ag-grid-renderer.module";
+
+import { AngularSplitModule } from 'angular-split';
 
 import { TreeModule } from "angular-tree-component";
 
-import { ButtonModule } from "../../modules/button.module";
-import { CheckBoxModule} from "../../modules/checkbox.module";
-import { ComboBoxModule }     from "../../modules/combobox.module";
-import { EmailRelatedUsersPopupModule } from "../util/emailRelatedUsersPopup/email-related-users-popup.module";
-import { ExpanderModule }     from "../../modules/expander.module";
-import { GnomexStyledGridModule } from "../util/gnomexStyledJqxGrid/gnomex-styled-grid.module";
-import { InputModule } from "../../modules/input.module";
-import { LoaderModule }       from "../../modules/loader.module";
-import { PanelModule }        from "../../modules/panel.module";
-import { NotificationModule } from "../../modules/notification.module";
-import { TextAreaModule }     from "../../modules/textarea.module";
-import { ToggleButtonModule } from "../../modules/togglebutton.module";
-import {WindowModule}       from "../../modules/window.module";
-import {ServicesModule} from "../services/services.module";
-import { AngularSplitModule } from 'angular-split';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {DialogsModule} from "../util/popup/dialogs.module";
-import {AngularMaterialModule} from "../../modules/angular-material.module";
-import {UtilModule} from "../util/util.module";
-
-import {AgGridModule} from 'ag-grid-angular/main';
-import {AgGridEditorModule} from "../util/grid-editors/ag-grid-editor.module";
-import {AgGridRendererModule} from "../util/grid-renderers/ag-grid-renderer.module";
+import { AngularMaterialModule } from "../../modules/angular-material.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DialogsModule } from "../util/popup/dialogs.module";
+import { ServicesModule } from "../services/services.module";
+import { UtilModule } from "../util/util.module";
 
 import { CheckboxRenderer } from "../util/grid-renderers/checkbox.renderer";
 import { IconLinkButtonRenderer } from "../util/grid-renderers/icon-link-button.renderer";
@@ -40,17 +27,16 @@ import { TextAlignLeftMiddleRenderer } from "../util/grid-renderers/text-align-l
 import { TextAlignRightMiddleRenderer } from "../util/grid-renderers/text-align-right-middle.renderer";
 import { UploadViewRemoveRenderer } from "../util/grid-renderers/upload-view-remove.renderer";
 
-import {USERS_GROUPS_ROUTING} from "./users-groups.routes";
-import {UsersGroupsTablistComponent} from "./users-groups-tablist.component";
-import {IconTextRendererComponent} from "../util/grid-renderers/icon-text-renderer.component";
-import {NewUserDialogComponent} from "./new-user-dialog.component";
-import {DeleteUserDialogComponent} from "./delete-user-dialog.component";
-import {NewGroupDialogComponent} from "./new-group-dialog.component";
-import {DeleteGroupDialogComponent} from "./delete-group-dialog.component";
-import {VerifyUsersDialogComponent} from "./verify-users-dialog.component";
-import {BillingAccountTabComponent} from "./billingAccountTab/billing-account-tab.component";
-import {BillingAdminTabComponent} from "./billingAdminTab/billing-admin-tab.component";
-import {MembershipTabComponent} from "./membershipTab/membership-tab.component";
+import { BillingAccountTabComponent } from "./billingAccountTab/billing-account-tab.component";
+import { BillingAdminTabComponent } from "./billingAdminTab/billing-admin-tab.component";
+import { MembershipTabComponent } from "./membershipTab/membership-tab.component";
+import { NewGroupDialogComponent } from "./new-group-dialog.component";
+import { NewUserDialogComponent } from "./new-user-dialog.component";
+import { DeleteGroupDialogComponent } from "./delete-group-dialog.component";
+import { DeleteUserDialogComponent } from "./delete-user-dialog.component";
+import { UsersGroupsTablistComponent } from "./users-groups-tablist.component";
+import { VerifyUsersDialogComponent } from "./verify-users-dialog.component";
+import { USERS_GROUPS_ROUTING } from "./users-groups.routes";
 /**
  * @author jdewell
  * @since 12/19/16
@@ -59,43 +45,27 @@ import {MembershipTabComponent} from "./membershipTab/membership-tab.component";
 @NgModule({
     imports: [
 			  AgGridEditorModule,
+			  AgGridModule.withComponents([
+			  	  TextAlignLeftMiddleRenderer,
+			  	  RemoveLinkButtonRenderer,
+			  	  CheckboxRenderer,
+			  	  IconLinkButtonRenderer,
+			  	  UploadViewRemoveRenderer,
+			  	  SelectEditor,
+			  	  SelectRenderer,
+			  	  TextAlignRightMiddleRenderer
+			  ]),
 			  AgGridRendererModule,
-        AngularMaterialModule,
-        DialogsModule,
-        BrowserAnimationsModule,
-        USERS_GROUPS_ROUTING,
-        ButtonModule,
-        CheckBoxModule,
-        ComboBoxModule,
-        CommonModule,
-        EmailRelatedUsersPopupModule,
-        ExpanderModule,
-        FormsModule,
-        ReactiveFormsModule,
-        GnomexStyledGridModule,
-        InputModule,
-        LoaderModule,
-        NotificationModule,
-        PanelModule,
-        ServicesModule,
-        TextAreaModule,
-        ToggleButtonModule,
-        TreeModule,
-        UtilModule,
-        WindowModule,
-        ReactiveFormsModule,
-        AngularSplitModule,
-        AgGridModule.withComponents([
-					  TextAlignLeftMiddleRenderer,
-            RemoveLinkButtonRenderer,
-            CheckboxRenderer,
-					  IconLinkButtonRenderer,
-            IconTextRendererComponent,
-					  UploadViewRemoveRenderer,
-					  SelectEditor,
-					  SelectRenderer,
-					  TextAlignRightMiddleRenderer
-        ])
+			  AngularMaterialModule,
+			  AngularSplitModule,
+			  BrowserAnimationsModule,
+			  DialogsModule,
+			  FormsModule,
+			  ReactiveFormsModule,
+			  ServicesModule,
+			  TreeModule,
+			  USERS_GROUPS_ROUTING,
+			  UtilModule
     ],
     declarations: [
         BillingAccountTabComponent,
