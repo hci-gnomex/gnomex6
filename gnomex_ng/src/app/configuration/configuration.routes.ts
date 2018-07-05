ba3a42658bd0246565a2bdd04dbaadf2551ed955
John Hofer <John.Hofer@hci.utah.edu>
2018-07-05 09:50:22 -0600

    Split protocols into two components, routing and improvements to spinners

--- gnomex_ng/src/app/configuration/configuration.routes.ts
+++ gnomex_ng/src/app/configuration/configuration.routes.ts
@@ -15,31 +15,11 @@ const ROUTES: Routes = [
     { path: "configure-annotations", component: ConfigureAnnotationsComponent },
     { path: "configure-annotations/:idCoreFacility", component: ConfigureAnnotationsComponent },
     { path: "configure-organisms", component: ConfigureOrganismsComponent },
-    // { path: "manage-protocols", component: ManageProtocolsComponent },
 
-
-    // {
-    //     path:      'manage-protocols',
-    //     component: ManageProtocolsComponent,
-    //     canActivate: [SubRouteGuardService]
-    // },
     {
-        // path:      "manage-protocols/:modelName",
         path:      "manage-protocols",
         component: ManageProtocolsComponent,
         children:  [
-            // {
-            //     path:       '',
-            //     pathMatch:  'full',
-            //     redirectTo: '/manage-protocols'
-            // },
-            // {
-            //     // path:      ':modelName/:id',
-            //     path:      'edit',
-            //     component: EditProtocolComponent,
-            //     outlet:    'browsePanel'
-            // }
-
             {
                 path: '',
                 pathMatch: 'full',
@@ -55,29 +35,7 @@ const ROUTES: Routes = [
                 component: EditProtocolComponent,
                 outlet: 'browsePanel'
             }
-            // {
-            //     path:':id',
-            //     component: ManageDictionaryComponent,
-            //     outlet: 'browsePanel'
-            // }
-
-            // {
-            //     path:       ':modelName',
-            //     pathMatch:  'full',
-            //     redirectTo: 'manage-protocols',
-            //     outlet:     'browsePanel',
-            //     children: [
-            //         {
-            //             path:      ':id',
-            //             component: EditProtocolComponent,
-            //             outlet:    'browsePanel'
-            //         }
-            //     ]
-            // }
         ],
-        // canActivate: [SubRouteGuardService],
-        // runGuardsAndResolvers: 'paramsChange',
-        // outlet: 'browsePanel'
     },
 ];
 
