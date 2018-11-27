e6304e58174f687787649fc03d9ca386b2ae1225
Michael Morris <michael.morris@hci.utah.edu>
2018-11-27 14:27:00 -0700

    Added web link sharing for analyses. (mmorris)

--- gnomex_ng/src/app/util/share-link-dialog.component.ts
+++ gnomex_ng/src/app/util/share-link-dialog.component.ts
@@ -1,58 +1,48 @@
-import {Component, ElementRef, Inject, OnInit} from "@angular/core";
+import {Component, Inject, OnInit} from "@angular/core";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder} from "@angular/forms";
 import {CreateSecurityAdvisorService} from "../services/create-security-advisor.service";
-import {DOCUMENT} from '@angular/platform-browser';
 
 @Component({
     template: `
-        
             <mat-dialog-content style="display: flex; flex-direction: column; height: 85%; position: relative">
-
                 <h6 mat-dialog-title>Web Link: {{this.name}}</h6>
                 <input style="margin: 1em;" disabled [(ngModel)]="link"/> 
                 <button mat-button color="primary" (click)="copyToClipboard(link)">
                     Copy To Clipboard
                 </button>
             </mat-dialog-content>
-      
-        
     `,
 })
 export class ShareLinkDialogComponent implements OnInit{
 
-    public link: string= '';
-    private number:string= '';
-    private type:string='';
+    public link: string = '';
+    private number:string = '';
+    private type:string ='';
     public name:string = '';
-
-
 
     constructor(private dialogRef: MatDialogRef<ShareLinkDialogComponent>,
                 @Inject(MAT_DIALOG_DATA) private data: any,
                 private fb:FormBuilder,
                 private secAdvisor:CreateSecurityAdvisorService,
-                @Inject(Window) private _window: Window ) {
-
+                @Inject(Window) private _window: Window) {
     }
 
-    ngOnInit(){
-
+    ngOnInit() {
         this.name = this.data.name;
         this.number = this.data.number;
         this.type = this.data.type;
         this.link = this.makeURL(this.type,this.number);
     }
 
-    makeURL(orderType:string,number:string):string{
-        //native javascript
-        let baseUrl = `https://${this._window.location.hostname}/gnomex/?${orderType+'='+number}`;
-        return baseUrl;
+    private makeURL(orderType:string, number:string): string {
+        // native javascript
+        return `${this._window.location.origin}/gnomex/?${orderType + '=' + number}`;
     }
 
-    copyToClipboard(text:string){
+    public copyToClipboard(text:string): void {
         //native javascript
-        var event = (e: ClipboardEvent) => {
+        let event = (e: ClipboardEvent) => {
             e.clipboardData.setData('text/plain', text);
             e.preventDefault();
             document.removeEventListener('copy', event);
