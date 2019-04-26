06ffd4e30217394489d348fc3d49c01071060211
Michael Morris <michael.morris@hci.utah.edu>
2019-04-26 10:08:26 -0600

    New experiment setup now slightly more property driven. (mmorris)

--- gnomex_ng/src/app/services/property.service.ts
+++ gnomex_ng/src/app/services/property.service.ts
@@ -44,6 +44,9 @@ export class PropertyService {
     public static readonly PROPERTY_HELP_URL: string = "help_url";
     public static readonly PROPERTY_HIDE_INSTITUTIONS: string = "hide_institutions";
     public static readonly PROPERTY_ALLOW_ADD_SEQUENCING_SERVICES: string = "allow_add_sequencing_services";
+    public static readonly PROPERTY_WORKAUTH_INSTRUCTIONS: string = "workauth_instructions";
+    public static readonly PROPERTY_AUTH_ACCOUNTS_DESCRIPTION: string = "auth_accounts_description";
+    public static readonly PROPERTY_ACCESS_AUTH_ACCOUNT_LINK_TEXT: string = "access_auth_account_link_text";
 
     public static readonly PROPERTY_ANALYSIS_ASSISTANCE_GROUP: string  = "analysis_assistance_group";
     public static readonly PROPERTY_ANALYSIS_ASSISTANCE_HEADER: string = "analysis_assistance_header";
@@ -183,8 +186,12 @@ export class PropertyService {
     }
 
     public getPropertyAsBoolean(name: string, idCoreFacility?: string, codeRequestCategory?: string): boolean {
+        return this.getPropertyValue(name, idCoreFacility, codeRequestCategory) === "Y";
+    }
+
+    public getPropertyValue(name: string, idCoreFacility?: string, codeRequestCategory?: string): string {
         let prop: any = this.getProperty(name, idCoreFacility, codeRequestCategory);
-        return prop && prop.propertyValue && prop.propertyValue === 'Y';
+        return prop && prop.propertyValue ? prop.propertyValue : "";
     }
 
 }
