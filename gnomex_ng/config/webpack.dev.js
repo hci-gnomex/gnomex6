0beb453c1485afbda3ea091876c0b0664f9a8f66
Michael Morris <michael.morris@hci.utah.edu>
2019-09-20 12:44:10 -0600

    Improve build process by refactoring gradle and webpack
    Performed general clean-up of build.gradle, package.json, and various
    webpack config files. Upgraded gradle wrapper version as well.

--- gnomex_ng/config/webpack.dev.js
+++ gnomex_ng/config/webpack.dev.js
@@ -1,53 +1,35 @@
-var commonConfig = require("./webpack.common.js");
 const DefinePlugin = require("webpack/lib/DefinePlugin");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var helpers = require("./helpers");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const Helpers = require("./helpers");
 const LoaderOptionsPlugin = require("webpack/lib/LoaderOptionsPlugin");
-var webpackMerge = require("webpack-merge");
-//var Visualizer = require('webpack-visualizer-plugin');
+//const Visualizer = require('webpack-visualizer-plugin');
 
-const ENV = process.env.NODE_ENV = process.env.ENV = "development";
-
-/**
- * Additional build information to webpack.common.js which doesn't include extra minification that
- * webpack.prod.js uses.
- *
- * @param options Just contains ENV for dev or prod builds.
- * @returns {*}
- */
-module.exports = function (options) {
-    return webpackMerge(commonConfig({env: ENV}), {
+module.exports = (options) => {
+    const Env = process.env.ENV = process.env.NODE_ENV = options.env;
+    return {
+        mode: 'development',
         devtool: "inline-source-map",
-
         output: {
-            path: helpers.root("dist"),
-            publicPath: "/",
+            path: Helpers.root("dist"),
+            publicPath: options.publicPath,
             filename: "[name].js",
             chunkFilename: "[id].chunk.js",
             sourceMapFilename: "[file].map"
         },
-
         plugins: [
             new ExtractTextPlugin("[name].css"),
-
             //new Visualizer(),
-
             new LoaderOptionsPlugin({
                 debug: true,
                 options: {}
             }),
-
             new DefinePlugin({
-                "ENV": JSON.stringify(ENV),
+                "ENV": JSON.stringify(Env),
                 "process.env": {
-                    "ENV": JSON.stringify(ENV),
-                    "NODE_ENV": JSON.stringify(ENV),
+                    "ENV": JSON.stringify(Env),
+                    "NODE_ENV": JSON.stringify(Env),
                 }
             }),
-
-
-
         ],
-
-    });
-}
+    };
+};
