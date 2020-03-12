de211b84af9dd01a984080bcb0d2dde3c414fbce
u0566434 <erik.rasmussen@hci.utah.edu>
2020-03-12 09:48:22 -0600

    TGNOM rsync4j

--- src/main/java/hci/gnomex/daemon/auto_import/StringModder.java
+++ src/main/java/hci/gnomex/daemon/auto_import/StringModder.java
@@ -1,15 +1,36 @@
 package hci.gnomex.daemon.auto_import;
 
+import com.github.fracpete.processoutput4j.output.CollectingProcessOutput;
+import com.github.fracpete.rsync4j.RSync;
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class StringModder {
 
-	public static void main(String[] args) {
-		List<String> stringList = new ArrayList<String>(); 
-		
+	public static void main(String[] args) throws Exception {
+		List<String> stringList = new ArrayList<String>(Arrays.asList("C:\\Users\\u0566434\\Desktop\\ORIEN\\Foundation\\Follow Up\\corrupted_bams.txt",
+				"C:\\Users\\u0566434\\Desktop\\ORIEN\\Foundation\\Follow Up\\mv_foundation_2_27-20.txt",
+				"C:\\Users\\u0566434\\Downloads\\1GB.zip",
+				"C:\\Users\\u0566434\\Downloads\\ideaIU-2018.1.5.exe"));
+
+
+
+		RSync rsync = new RSync()
+				.sources(stringList)
+				.destination("X:\\TEMP\\tempTest")
+				.recursive(true);
+
+		CollectingProcessOutput output = rsync.execute();
+		System.out.println(output.getStdOut());
+		System.out.println("Exit code: " + output.getExitCode());
+		if (output.getExitCode() > 0)
+			System.err.println(output.getStdErr());
+
+
 		Scanner scan = new Scanner(System.in);
 		String str = "";
 		
